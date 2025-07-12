79ad0404fe5a8876e1c35497677c0c037fd0c225
chalapathi <harijanachalapathi7@gmail.com>
2025-07-12 14:41:43 +0530

    done

--- Spring_boot_Country_Service_proj/src/main/java/com/example/demo/beans/Common.java
+++ Spring_boot_Country_Service_proj/src/main/java/com/example/demo/beans/Common.java
@@ -2,6 +2,6 @@ package com.example.demo.beans;
 
 public class Common {
 	public void abc() {
-System.out.println("branch for conficts");
+System.out.println("branch for conficts number 1 ");
 	}
 }
